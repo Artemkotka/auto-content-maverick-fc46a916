e585ceb08a55e87f38df54585d2527fc0ea26c8d
Artemkotka <artemkotka@gmail.com>
2025-03-28 10:47:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+base: "/auto-content-maverick-fc46a916/"
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
